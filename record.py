f3533e42509b81db349175af646792605e4c1bca
Nileshkumarg <nileshkumargupta412@gmail.com>
2022-12-09 15:53:34 +0530

    first Commit

---
+++ record.py
@@ -0,0 +1,49 @@
+import cv2
+from datetime import datetime
+import time
+
+def record():
+    cap = cv2.VideoCapture(0)
+
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(f'recordings/{datetime.now().strftime("%H-%M-%S")}.avi', fourcc,20.0,(640,480))
+
+    while True:
+        _, frame = cap.read()
+
+        cv2.putText(frame, f'{datetime.now().strftime("%D-%H-%M-%S")}', (50,50), cv2.FONT_HERSHEY_COMPLEX,
+                        0.6, (255,255,255), 2)
+
+        out.write(frame)
+        
+
+        cv2.imshow("Record|WatchDog", frame)
+
+        if cv2.waitKey(1) == 27:
+            cap.release()
+            cv2.destroyAllWindows()
+            break 
+
+def rem_record(stop_event=1):
+    cap = cv2.VideoCapture(0)
+    start = time.time()
+
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(f'recordings/{datetime.now().strftime("%H-%M-%S")}.avi', fourcc,20.0,(640,480))
+
+    while True:
+        _, frame = cap.read()
+
+        cv2.putText(frame, f'{datetime.now().strftime("%D-%H-%M-%S")}', (50,50), cv2.FONT_HERSHEY_COMPLEX,
+                        0.6, (255,255,255), 2)
+
+        out.write(frame)
+        
+
+        cv2.imshow("Record|WatchDog", frame)
+
+
+        if cv2.waitKey(1) == 27 or time.time() - start >= stop_event*10:    
+            cap.release()
+            cv2.destroyAllWindows()
+            break
